e32692bfa3283198036c61f8005c08e4198bad31
utopiaexpress123 <ricksanchezy2x@gmail.com>
2024-05-29 19:51:44 +0200

    yxy

--- components/history.tsx
+++ components/history.tsx
@@ -12,6 +12,7 @@ import { HistoryList } from './history-list'
 import { Suspense } from 'react'
 import { HistorySkeleton } from './history-skelton'
 
+
 type HistoryProps = {
   location: 'sidebar' | 'header'
 }
@@ -48,9 +49,12 @@ export function History({ location }: HistoryProps) {
 }
 
 
+
 function HistoryIcon() {
   return (
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 rounded-full hover:shadow-lg">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
 </svg>
+
   )
+}
