61a6d3f9c287972846d2ef88afe8bf15b24d9dd6
utopiaexpress123 <ricksanchezy2x@gmail.com>
2024-05-29 14:55:51 +0200

    Update page.tsx

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignIn />;
+  return <div className="grid h-screen grid-cols-1 place-items-center">
+            <div className="">
+              <SignIn />
+            </div> 
+        </div>;
 }
