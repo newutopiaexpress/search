cebc61fea48dbd4765364d5d07ef9f133ee0f5cc
utopiaexpress123 <ricksanchezy2x@gmail.com>
2024-06-28 15:11:22 +0200

    wew

--- components/header.tsx
+++ components/header.tsx
@@ -16,21 +16,23 @@ import { ThemeProvider } from '@/components/theme-provider'
 export const Header: React.FC = async () => {
   return (
     <header className="fixed w-full p-1 md:p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
-      <div className='opacity-85 hover:opacity-100 mt-2 ml-2 md:mt-4 md:ml-4'>
-        <a href="/">
-          <UtopiaIcon/>
-        </a>
-      </div>
+      <div className="relative">
+        <div className='opacity-85 hover:opacity-100 mt-2 ml-2 md:mt-4 md:ml-4'>
+          <a href="/">
+            <UtopiaIcon/>
+          </a>
+        </div>
 
-      <div className="absolute right-4 top-4">
-            <SignedIn>
-            {/* Mount the UserButton component */}
-            <UserButton />
-            </SignedIn>
-      </div>
+        <div className="absolute right-4 top-4">
+              <SignedIn>
+              {/* Mount the UserButton component */}
+              <UserButton />
+              </SignedIn>
+        </div>
 
-      <div className="flex gap-0.5">
-        <HistoryContainer location="header" />
+        <div className="flex gap-0.5">
+          <HistoryContainer location="header" />
+        </div>
       </div>
     </header>
   )
